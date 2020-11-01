e78344f08928fca1923993ea61a592ca13f78fb8
Kitaya Kaito <kworkshere@gmail.com>
2020-11-01 15:29:10 +0900

    tryal2

--- main.py
+++ main.py
@@ -55,7 +55,6 @@ def handle_message(event):
         event.reply_token,
 #        TextSendMessage(text=event.message.text)
         TextSendMessage(text="リマインダを起動しました\nキーワードを入力して通知時刻を設定してください。例：1月1日0時0分→1,1,0,0"),
-        TextSendMessage(text=str(lis))
         )
 
 
