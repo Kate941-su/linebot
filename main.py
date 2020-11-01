12f0daee26cfee5e84c74d7b0da08618bf707b9c
Kitaya Kaito <kworkshere@gmail.com>
2020-11-01 14:49:09 +0900

    tryal2

--- main.py
+++ main.py
@@ -51,8 +51,11 @@ def callback():
 def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
+        lis=["hello"]
+#        lis.append(event.message.text)
 #        TextSendMessage(text=event.message.text)
         TextSendMessage(text="リマインダを起動しました\nキーワードを入力して通知時刻を設定してください。例：1月1日0時0分→1,1,0,0")
+        TextSendMessage(text=lis)
         )
 
 
