e18498bfab9a1c2dd84f0ac17779017d6be61a80
Kaito Kitaya <ajustandrungao@yahoo.co.jp>
2020-11-02 13:00:04 +0900

    ちょっと変えた

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from linebot import LineBotApi, WebhookHandler
 
 from linebot.exceptions import  InvalidSignatureError
 
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ButtonsTemplate
 
 import datetime
 
