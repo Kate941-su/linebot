2f2172ed4ae8db0028a9a8fdde9c7d1750a9862f
Kitaya Kaito <kworkshere@gmail.com>
2020-11-04 21:45:55 +0900

    Nov4

--- randw.py
+++ randw.py
@@ -1,25 +1,34 @@
-import xlrd
+import xlrd,xlwt
+import openpyxl
 import pprint
-
-wb=xlrd.open_workbook("planandday1.xls")#open xls file(wb=work book)
+from random import randint
+wb=xlrd.open_workbook("planandday1.xlsx")#open xls file(wb=work book)
 ws = wb.sheet_by_name('plan')#get sheet data(ws=work sheet)
 cell = ws.cell(1,2)
 print(cell)
 print(cell.value)
 
-print(ws.col(0))#get col data
-print(ws.row(0))#get row data
-col_end=len(ws.col(0))
-row_end=len(ws.row(0))
+col_end=ws.ncols
+row_end=ws.nrows
+
+print(ws.col(0))
 
 print(col_end,row_end)
-latest_id=ws.cell(col_end-1,row_end-1).value
+latest_id=ws.cell(row_end-1,col_end-1).value
 latest_id=int(latest_id)#cast float -> int
 print(latest_id)#get latest id
 
-wb=xlrd.open_workbook("planandday1.xls")
+wb=xlrd.open_workbook("planandday1.xlsx")
 ws = wb.sheet_by_name('plan')
 row_end=len(ws.row(0))
 col_end=len(ws.col(0))
+buffer_col=7
 latest_row=ws.cell(col_end-1,row_end-2).value
 latest_row=int(latest_row)#cast float -> int
+print(latest_row)
+issue_id=randint(0,10000)
+
+wb=openpyxl.load_workbook("planandday1.xlsx")
+ws=wb.worksheets[0]
+ws.cell(row=latest_row+1,column=buffer_col,value="sample")
+wb.save("planandday1.xlsx")
