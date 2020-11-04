126ec786c5285d493416e9d81a8b0cd134cd983f
Kitaya Kaito <kworkshere@gmail.com>
2020-11-04 21:59:36 +0900

    rename

--- randw.py
+++ randw.py
@@ -2,7 +2,7 @@ import xlrd,xlwt
 import openpyxl
 import pprint
 from random import randint
-wb=xlrd.open_workbook("planandday1.xlsx")#open xls file(wb=work book)
+wb=xlrd.open_workbook("sample.xlsx")#open xls file(wb=work book)
 ws = wb.sheet_by_name('plan')#get sheet data(ws=work sheet)
 cell = ws.cell(1,2)
 print(cell)
@@ -18,7 +18,7 @@ latest_id=ws.cell(row_end-1,col_end-1).value
 latest_id=int(latest_id)#cast float -> int
 print(latest_id)#get latest id
 
-wb=xlrd.open_workbook("planandday1.xlsx")
+wb=xlrd.open_workbook("sample.xlsx")
 ws = wb.sheet_by_name('plan')
 row_end=len(ws.row(0))
 col_end=len(ws.col(0))
@@ -28,7 +28,7 @@ latest_row=int(latest_row)#cast float -> int
 print(latest_row)
 issue_id=randint(0,10000)
 
-wb=openpyxl.load_workbook("planandday1.xlsx")
+wb=openpyxl.load_workbook("sample.xlsx")
 ws=wb.worksheets[0]
-ws.cell(row=latest_row+1,column=buffer_col,value="sample")
-wb.save("planandday1.xlsx")
+#ws.cell(row=latest_row+1,column=buffer_col,value="sample")
+wb.save("sample.xlsx")
