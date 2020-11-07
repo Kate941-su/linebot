27d97014d4bf121ad47322c3513d3614130be799
Kitaya Kaito <kworkshere@gmail.com>
2020-11-07 17:13:34 +0900

    test deploy

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def response_message(event):
                 else:
                     line_bot_api.reply_message(
                     event.reply_token,
-                    TextSendMessage(text="入力ミスがあります。\n日付を教えてください\nex)明日、今日、明後日、11月6日"),
+                    TextSendMessage(text=ws.cell(row=2,column=buffer1).value),
                     )
                     ws_w.cell(row=2,column=flag,value=1)
 #Flag2 phase
