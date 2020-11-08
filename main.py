0aa8b86201a6e90974d99a4f84cb597039188a5f
Kitaya Kaito <kworkshere@gmail.com>
2020-11-08 14:22:37 +0900

    test deploy

--- main.py
+++ main.py
@@ -83,7 +83,7 @@ def response_message(event):
 #入力ミス防止    
     error_flag=0
     Flag=0#条件分岐のためのflag
-    pattern = r'[12]\d{3}[/\-年](0?[1-9]|1[0-2])[/\-月](0?[1-9]|[12][0-9]|3[01])日?$'#日付一致の正規表現
+    pattern = r'(0?[1-9]|1[0-2])[/\-月](0?[1-9]|[12][0-9]|3[01])日?$'#日付一致の正規表現
 
     issue_id=randint(0,1000)
     wb=px.load_workbook("sample1.xlsx")#open xls file(wb=work book)
