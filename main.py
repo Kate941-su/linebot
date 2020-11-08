465e485ee57775247a7e7dac20a95be50ece2a47
Kitaya Kaito <kworkshere@gmail.com>
2020-11-08 14:20:34 +0900

    test deploy

--- main.py
+++ main.py
@@ -165,7 +165,7 @@ def response_message(event):
                 else:
                     line_bot_api.reply_message(
                     event.reply_token,
-                    TextSendMessage(text=str(type(ws.cell(row=2,column=buffer1).value) is datetime)+str(ws.cell(row=2,column=buffer1).value))
+                    TextSendMessage(text=bool(re.match(pattern,ws.cell(row=2,column=buffer1).value)))
                     ),
                     ws_w.cell(row=2,column=flag,value=1)
                     ws_w.cell(row=2,column=mistake,value=Mistake+1)
