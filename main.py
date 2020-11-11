752ea0d5fa5eecf59f41cc24e6b780c10b225b7d
Kitaya Kaito <kworkshere@gmail.com>
2020-11-12 00:55:14 +0900

    test deploy

--- main.py
+++ main.py
@@ -94,164 +94,159 @@ def response_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
     User_id=profile.user_id
 
-#fileの有無　あればそれを
-# 開くなければつくってそれを開く
-
+#fileの有無　あればそれを開くなければつくってそれを開く
     if os.path.exists("user"+str(User_id)+".xlsx"):
-        pass
+        wb=px.load_workbook("user"+str(User_id)+".xlsx")#open xls file(wb=work book)
+        ws = wb["plan"]#get sheet data(ws=work sheet)
+        wb_w=px.load_workbook("user"+str(User_id)+".xlsx")
+        ws_w=wb_w.worksheets[0]
+        ws_w.cell(row=b_row,column=flag,value=0)
+        ws_w.cell(row=b_row,column=mistake,value=0)
+        ws_w.cell(row=b_row,column=send_id,value=User_id)
+        wb_w.save("user"+str(User_id)+"xlsx")
+        wb=px.load_workbook("user"+str(User_id)+".xlsx")#open xls file(wb=work book)
+        ws = wb["plan"]#get sheet data(ws=work sheet)
+        Flag=int(ws.cell(row=b_row,column=flag).value)
+        Mistake=int(ws.cell(row=b_row,column=mistake).value)
+
+
+
+        if int(ws.cell(row=b_row,column=mistake).value)==2:
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="try again at first!!"),
+            )
+            ws_w.cell(row=b_row,column=mistake,value=0)
+            ws_w.cell(row=b_row,column=flag,value=0)
+
+    #Flag1 phase
+
+        else:
+            if Flag==0:
+                if event.message.text == "予約":
+                    line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="予約を行います日付を教えてください\nex)明日、今日、明後日、11月6日\n※予約時刻は10分単位で行います1分単位で予約すると10分繰り上げ通知となります"),               
+                )
+                    ws_w.cell(row=b_row,column=flag,value=1)
+                else:
+                    line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="予約したいときは”予約と入力してください”"),
+                )
+                    ws_w.cell(row=b_row,column=flag,value=0)
+
+            elif Flag==1:
+
+                is_message_date = event.message.text
+                ws_w.cell(row=b_row,column=buffer1,value=is_message_date)
+                wb_w.save("user"+str(User_id)+".xlsx")
+                wb=px.load_workbook("user"+str(User_id)+".xlsx")#open xls file(wb=work book)
+                ws = wb["plan"]#get sheet data(ws=work sheet)
+                #型を判定する
+                #datetime型で方が一致していた時
+                if str(ws.cell(row=b_row,column=buffer1).value) == "今日":   
+                    line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="何時何分に設定しますか\n入力フォーマット例(11時00分のとき):11:00（半角）\n※10分単位です!"),
+                    )
+                    ws_w.cell(row=b_row,column=flag,value=2)
+                    ws_w.cell(row=b_row,column=buffer1,value=event.message.text)#issue id
+
+                elif str(ws.cell(row=b_row,column=buffer1).value) == "明日":   
+                    line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="何時何分に設定しますか\n入力フォーマット例(11時10分のとき):11:10（半角）"),
+                    )
+                    ws_w.cell(row=b_row,column=flag,value=2)
+                    ws_w.cell(row=b_row,column=buffer1,value=event.message.text)#issue id
+
+                elif str(ws.cell(row=b_row,column=buffer1).value) == "明後日":   
+                    line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="何時何分に設定しますか\n入力フォーマット例(11時10分のとき):11:10（半角）"),
+                    )
+                    ws_w.cell(row=b_row,column=flag,value=2)
+                    ws_w.cell(row=b_row,column=buffer1,value=event.message.text)#issue id
+
+                    #今日明日明後日意外の処理
+                else:
+                    
+                    if bool(re.match(pattern,ws.cell(row=b_row,column=buffer1).value)):
+                        line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text="何時何分に設定しますか\n入力フォーマット例(11時10分のとき):11:10（半角）\n"),
+                        )
+                        ws_w.cell(row=b_row,column=flag,value=2)
+                        ws_w.cell(row=b_row,column=buffer1,value=event.message.text)#issue id
+                
+                    else:
+                        line_bot_api.reply_message(
+                        event.reply_token,
+                        TextSendMessage(text="入力ミスがあります。このような間違えはありませんか？\n数字が半角、月日をどちらか抜かしている")
+                        ),
+                        ws_w.cell(row=b_row,column=flag,value=1)
+                        ws_w.cell(row=b_row,column=mistake,value=Mistake+1)
+
+    #Flag2 phase
+
+            elif Flag==2:
+                r_message=event.message.text           
+                try:
+                    k=0
+                    r_message=r_message.split(":")
+                    for i in r_message:
+                        r_message[k]=int(i)
+                        k+=1
+                    if r_message[0]>=25 or r_message[1]>=60:
+                        error_flag=1
+                except:
+                    error_flag=1
+
+
+                if error_flag == 0:    
+                    line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="何の予定がありますか？\n"),
+                    )
+                    ws_w.cell(row=b_row,column=flag,value=3)
+                    ws_w.cell(row=b_row,column=buffer2,value=event.message.text)#issue id
+
+                else:
+                    line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text="入力ミスがあります。\n何時何分に設定しますか\n入力フォーマット例(11時10分のとき):11:10（半角）"),
+                    )
+                    ws_w.cell(row=b_row,column=flag,value=2)
+    #               ws_w.cell(row=b_row,column=buffer1,value=event.message.text)
+                    ws_w.cell(row=b_row,column=mistake,value=Mistake+1)
+    #Flag3 phase
+
+            elif Flag == 3:
+                ws_w.cell(row=b_row,column=buffer3,value=event.message.text)#issue id
+                ws_w.cell(row=b_row,column=flag,value=0)
+                ws_w.cell(row=b_row,column=send_id,value=profile.user_id)
+                wb_w.save("user"+str(User_id)+".xlsx")
+                wb=px.load_workbook("user"+str(User_id)+".xlsx")#open xls file(wb=work book)
+                ws = wb["plan"]#get sheet data(ws=work sheet)
+                line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=str(ws.cell(row=b_row,column=buffer1).value)+str(ws.cell(row=b_row,column=buffer2).value)+"に"+"”"+str(ws.cell(row=b_row,column=buffer3).value)+"”"+"で予約しました。\n"),        
+            )
+
+    #            ws_w.cell(row=b_row,column=buffer3,value=event.message.text)#issue id
+    #            ws_w.cell(row=b_row,column=flag,value=0) 
+        
+        wb_w.save("user"+str(User_id)+".xlsx")
+
     else:
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="登録までお待ちください"),
         ) 
-        user_id = os.environ["USER_ID"]
-        line_bot_api.push_message(user_id, TextSendMessage(text=user_id))
-
-    wb=px.load_workbook("user"+str(User_id)+".xlsx")#open xls file(wb=work book)
-    ws = wb["plan"]#get sheet data(ws=work sheet)
-    wb_w=px.load_workbook("user"+str(User_id)+".xlsx")
-    ws_w=wb_w.worksheets[0]
-    ws_w.cell(row=b_row,column=flag,value=0)
-    ws_w.cell(row=b_row,column=mistake,value=0)
-    ws_w.cell(row=b_row,column=send_id,value=User_id)
-    wb_w.save("user"+str(User_id)+"xlsx")
-    wb=px.load_workbook("user"+str(User_id)+".xlsx")#open xls file(wb=work book)
-    ws = wb["plan"]#get sheet data(ws=work sheet)
-    Flag=int(ws.cell(row=b_row,column=flag).value)
-    Mistake=int(ws.cell(row=b_row,column=mistake).value)
-
-
-
-    if int(ws.cell(row=b_row,column=mistake).value)==2:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="try again at first!!"),
+        user_id = os.environ["MY_ID"]
+        line_bot_api.push_message(user_id, TextSendMessage(text=user_id)
         )
-        ws_w.cell(row=b_row,column=mistake,value=0)
-        ws_w.cell(row=b_row,column=flag,value=0)
-
-#Flag1 phase
-
-    else:
-        if Flag==0:
-            if event.message.text == "予約":
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="予約を行います日付を教えてください\nex)明日、今日、明後日、11月6日\n※予約時刻は10分単位で行います1分単位で予約すると10分繰り上げ通知となります"),               
-            )
-                ws_w.cell(row=b_row,column=flag,value=1)
-            else:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="予約したいときは”予約と入力してください”"),
-            )
-                ws_w.cell(row=b_row,column=flag,value=0)
-
-        elif Flag==1:
-
-            is_message_date = event.message.text
-            ws_w.cell(row=b_row,column=buffer1,value=is_message_date)
-            wb_w.save("user"+str(User_id)+".xlsx")
-            wb=px.load_workbook("user"+str(User_id)+".xlsx")#open xls file(wb=work book)
-            ws = wb["plan"]#get sheet data(ws=work sheet)
-            #型を判定する
-            #datetime型で方が一致していた時
-            if str(ws.cell(row=b_row,column=buffer1).value) == "今日":   
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="何時何分に設定しますか\n入力フォーマット例(11時00分のとき):11:00（半角）\n※10分単位です!"),
-                )
-                ws_w.cell(row=b_row,column=flag,value=2)
-                ws_w.cell(row=b_row,column=buffer1,value=event.message.text)#issue id
-
-            elif str(ws.cell(row=b_row,column=buffer1).value) == "明日":   
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="何時何分に設定しますか\n入力フォーマット例(11時10分のとき):11:10（半角）"),
-                )
-                ws_w.cell(row=b_row,column=flag,value=2)
-                ws_w.cell(row=b_row,column=buffer1,value=event.message.text)#issue id
-
-            elif str(ws.cell(row=b_row,column=buffer1).value) == "明後日":   
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="何時何分に設定しますか\n入力フォーマット例(11時10分のとき):11:10（半角）"),
-                )
-                ws_w.cell(row=b_row,column=flag,value=2)
-                ws_w.cell(row=b_row,column=buffer1,value=event.message.text)#issue id
-
-                #今日明日明後日意外の処理
-            else:
-                
-                if bool(re.match(pattern,ws.cell(row=b_row,column=buffer1).value)):
-                    line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text="何時何分に設定しますか\n入力フォーマット例(11時10分のとき):11:10（半角）\n"),
-                    )
-                    ws_w.cell(row=b_row,column=flag,value=2)
-                    ws_w.cell(row=b_row,column=buffer1,value=event.message.text)#issue id
-              
-                else:
-                    line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text="入力ミスがあります。このような間違えはありませんか？\n数字が半角、月日をどちらか抜かしている")
-                    ),
-                    ws_w.cell(row=b_row,column=flag,value=1)
-                    ws_w.cell(row=b_row,column=mistake,value=Mistake+1)
-
-#Flag2 phase
-
-        elif Flag==2:
-            r_message=event.message.text           
-            try:
-                k=0
-                r_message=r_message.split(":")
-                for i in r_message:
-                    r_message[k]=int(i)
-                    k+=1
-                if r_message[0]>=25 or r_message[1]>=60:
-                    error_flag=1
-            except:
-                error_flag=1
-
-
-            if error_flag == 0:    
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="何の予定がありますか？\n"),
-                )
-                ws_w.cell(row=b_row,column=flag,value=3)
-                ws_w.cell(row=b_row,column=buffer2,value=event.message.text)#issue id
-
-            else:
-                line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="入力ミスがあります。\n何時何分に設定しますか\n入力フォーマット例(11時10分のとき):11:10（半角）"),
-                )
-                ws_w.cell(row=b_row,column=flag,value=2)
- #               ws_w.cell(row=b_row,column=buffer1,value=event.message.text)
-                ws_w.cell(row=b_row,column=mistake,value=Mistake+1)
-#Flag3 phase
-
-        elif Flag == 3:
-            ws_w.cell(row=b_row,column=buffer3,value=event.message.text)#issue id
-            ws_w.cell(row=b_row,column=flag,value=0)
-            ws_w.cell(row=b_row,column=send_id,value=profile.user_id)
-            wb_w.save("user"+str(User_id)+".xlsx")
-            wb=px.load_workbook("user"+str(User_id)+".xlsx")#open xls file(wb=work book)
-            ws = wb["plan"]#get sheet data(ws=work sheet)
-            line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=str(ws.cell(row=b_row,column=buffer1).value)+str(ws.cell(row=b_row,column=buffer2).value)+"に"+"”"+str(ws.cell(row=b_row,column=buffer3).value)+"”"+"で予約しました。\n"),        
-           )
-
-#            ws_w.cell(row=b_row,column=buffer3,value=event.message.text)#issue id
-#            ws_w.cell(row=b_row,column=flag,value=0) 
-    
-    wb_w.save("user"+str(User_id)+".xlsx")
-
-     
-
 
 
 #error時の対応
