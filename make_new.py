d20d5dc50db02b8ce5d02180b2dc6e18146a42e2
Kitaya Kaito <kworkshere@gmail.com>
2020-11-12 01:32:27 +0900

    adf

--- make_new.py
+++ make_new.py
@@ -11,15 +11,14 @@ def make_or_open_file(EVENT):
         wb=px.Workbook()
         ws=wb.active
         ws.title="plan"
-        wb.save("./user"+str(EVENT)+".xlsx")
-        print("make new")
-        wb=px.load_workbook("./user"+str(EVENT)+".xlsx")#open xls file(wb=work book)
-        ws = wb["plan"]#get sheet data(ws=work sheet)
-        wb_w=px.load_workbook("./user"+str(EVENT)+".xlsx")
-        ws_w=wb_w.worksheets[0]
-        ws_w.cell(row=2,column=10,value=0)
-        ws_w.cell(row=2,column=11,value=User_id)
-        wb_w.save("./user"+str(EVENT)+".xlsx")
+    wb.save("./user"+str(EVENT)+".xlsx")
+    wb=px.load_workbook("./user"+str(EVENT)+".xlsx")#open xls file(wb=work book)
+    ws = wb["plan"]#get sheet data(ws=work sheet)
+    wb_w=px.load_workbook("./user"+str(EVENT)+".xlsx")
+    ws_w=wb_w.worksheets[0]
+    ws_w.cell(row=2,column=10,value=0)
+    ws_w.cell(row=2,column=11,value=User_id)
+    wb_w.save("./user"+str(EVENT)+".xlsx")
 if __name__ ==  "__main__":
     make_or_open_file(event)
     
