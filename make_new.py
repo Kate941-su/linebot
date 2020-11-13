b2e97ca236eafbb4f8dac8484b63e87bd6e822e2
Kitaya Kaito <kworkshere@gmail.com>
2020-11-13 22:49:09 +0900

    test deploy

--- make_new.py
+++ make_new.py
@@ -16,8 +16,8 @@ def make_or_open_file(EVENT):
     ws = wb["plan"]#get sheet data(ws=work sheet)
     wb_w=px.load_workbook("./user"+str(EVENT)+".xlsx")
     ws_w=wb_w.worksheets[0]
-    ws_w.cell(row=200,column=1,value=0)
-    ws_w.cell(row=201,column=2,value=User_id)
+    for i in range(100):
+        ws_w.cell(row=201,column=i+1,value=0)    
     wb_w.save("./user"+str(EVENT)+".xlsx")
 if __name__ ==  "__main__":
     make_or_open_file(event)
