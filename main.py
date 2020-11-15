a2d6fff6bc578bc2f8eeff8deca9ca772161cf6b
Kitaya Kaito <kworkshere@gmail.com>
2020-11-15 23:26:22 +0900

    test deploy

--- main.py
+++ main.py
@@ -383,7 +383,7 @@ def response_message(event):
                     if type(ws.cell(row=b_row,column=buffer1).value) == type("hello"):#buffer1の内容はb_rowからissue_idに移していない
                         line_bot_api.reply_message(
                         event.reply_token,
-                        TextSendMessage(text=str(ws.cell(row=issue_id,column=buffer1).value)+"の"+str(ws.cell(row=issue_id,column=hh).value)+"時"+str(ws.cell(row=issue_id,column=mm).value)+"分に"+"”"+str(ws.cell(row=b_row,column=buffer3).value)+"”"+"で予約しました。\n"+str(issue_id)),        
+                        TextSendMessage(text=str(ws.cell(row=b_row,column=buffer1).value)+"の"+str(ws.cell(row=issue_id,column=hh).value)+"時"+str(ws.cell(row=issue_id,column=mm).value)+"分に"+"”"+str(ws.cell(row=b_row,column=buffer3).value)+"”"+"で予約しました。\n"+str(issue_id)),        
                         )
                     else:
                         line_bot_api.reply_message(
