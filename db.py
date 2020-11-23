acf6150e6e24ebfd4e349a4a85972c37f7853abd
Kitaya Kaito <kworkshere@gmail.com>
2020-11-23 10:53:17 +0900

    test deploy

--- db.py
+++ db.py
@@ -1,12 +1,22 @@
 import psycopg2 as p2
 import psycopg2.extras
-import os
+
 from datetime import datetime
 from datetime import timedelta
+from flask import Flask, request, abort
+
+import os
+
+from linebot import LineBotApi, WebhookHandler
+
+from linebot.exceptions import  InvalidSignatureError
+
+from linebot.models import MessageEvent, TextMessage, TextSendMessage,TemplateSendMessage,ButtonsTemplate,MessageAction
 
 
-
-
+#環境変数取得
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 
 Host=os.environ.get('PG_HOST')
 Port=os.environ.get('PG_PORT')
@@ -15,27 +25,23 @@ User=os.environ.get('PG_USER')
 Password=os.environ.get('PG_PASS')
 user_id=os.environ.get('USER_ID')
 connection = p2.connect("host="+str(Host)+" port="+str(Port)+" dbname="+str(Database)+" user="+str(User)+" password="+str(Password))
-
-send_id=os.environ.get("SEND_ID")
-
 #自動トランザクション
 connection.autocommit = True
-print(connection.get_backend_pid())
-
-context = "{}"
-context = context.format("id int,data text,day date,min time")
+#print(connection.get_backend_pid())
+#context = "{}"
+#context = context.format("id int,data text,day date,min time")
 #これによってSQLオブジェクトを使用可能にする
 cur = connection.cursor()
 #print(cur)
-val=1
-char=send_id
-day='2020/11/20'
-tim='11:11'
-cur.execute("create table if not exists "+str("hello")+"demo("+context+");")
+#val=1
+#char=send_id
+#day='2020/11/20'
+#tim='11:11'
+#cur.execute("create table if not exists "+str("hello")+"demo("+context+");")
 #print(cur.execute("select count (*) from hellodemo;"))
 #cur.execute("insert into hellodemo values(NULL,'HELLO MMA','2020/11/19','11:11');")
 #py変数の代入
-cur.execute("insert into "+str("hello")+"demo values(%s,%s,%s,%s);",(val,char,day,tim))
+#cur.execute("insert into "+str("hello")+"demo values(%s,%s,%s,%s);",(val,char,day,tim))
 #cur.execute("delete from hellodemo where id =1")
 #cur.execute("delete from hellodemo where id =2")
 
@@ -45,28 +51,37 @@ connection.commit()
 #SQL文の実行
 #cur.execute("SELECT * FROM "+str(send_id)+";")
 #results= cur.fetchall()
-
 dictcur = connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
-dictcur.execute("SELECT * FROM "+str(send_id)+";")
-result_dict=dictcur.fetchall()
-dict_result = []
-#ここのループで辞書にしている
-for row in result_dict:
-    dict_result.append(dict(row))
-len_dic=len(result_dict)
 
-for row in range(len_dic):
-  year=dict_result[row]["yyyy"]  
-  month=dict_result[row]["mm"]
-  day=dict_result[row]["dd"]
-  hour=dict_result[row]["hh"]
-  minute=dict_result[row]["minute"]
-  #日本時間に合わせる作業
-  nowon=datetime.now()
-  nowon+=timedelta(hours=9)
-  plan_date=datetime(year=year,month=month,day=day,hour=hour,minute=minute)
+#DB内テーブル取得
+cur.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
+each_table=cur.fetchall()
+for i in each_table:
+  send_id=str(i[0])
+  dictcur.execute("SELECT * FROM "+send_id+";")
+  result_dict=dictcur.fetchall()
+  dict_result = []
+  #ここのループで辞書にしている
+  for row in result_dict:
+      dict_result.append(dict(row))
+  len_dic=len(result_dict)
 
-  if nowon >= plan_date:
+  for row in range(len_dic):
+    plan=dict_result[row]["plan"]
+    year=dict_result[row]["yyyy"]  
+    month=dict_result[row]["mm"]
+    day=dict_result[row]["dd"]
+    hour=dict_result[row]["hh"]
+    minute=dict_result[row]["mmmm"]
+    issue_id=dict_result[row]["issue_id"]
+    #日本時間に合わせる作業
+    nowon=datetime.now()
+    nowon+=timedelta(hours=9)
+    plan_date=datetime(year=year,month=month,day=day,hour=hour,minute=minute)
+
+    if nowon >= plan_date:
+      line_bot_api.push_message(send_id, TextSendMessage(text=str(plan)+"の時間です。"))
+      dictcur.execute("delete from "+send_id+" where issue_id="+str(issue_id))
 
 
 #print(len_dic)
