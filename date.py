6d87c3f97cf796f3dfcfd1456f246663e5dbff58
Kate941-su <62321671+Kate941-su@users.noreply.github.com>
2021-01-15 19:31:39 +0900

    Delete date.py

--- date.py
+++
@@ -1,9 +0,0 @@
-from datetime import date,datetime,timedelta
-import re
-month=datetime.now().month
-day=datetime.now().day
-hour=datetime.now().hour
-minute=datetime.now().minute
-
-print(month,day,hour,minute)
-
